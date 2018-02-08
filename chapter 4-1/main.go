9028b55d46c062f685a90e4d6bc9480823111baf
tn55006 <anupong.pe@tnis.com>
2018-02-08 10:13:27 +0700

    update chapter 4-1 multiple var, swap

--- chapter 4-1/main.go
+++ chapter 4-1/main.go
@@ -22,4 +22,22 @@ fmt.Printf("Type z: %T\n", z)
 fmt.Printf("Type a: %T\n", a)
 fmt.Printf("Type b: %T\n", b)
 //fmt.Printf("Type c: %T\n", c)
+
+const xx string = "Hello, World"
+//cannot assign xx = "Other string"
+
+//Multiple var
+var(
+	s=5
+	t=10
+	u=15
+	)
+fmt.Println(s,t,u)
+
+v1,v2 := "fist", "sec"
+fmt.Println(v1,v2)
+
+//swap
+v1, v2 = v2, v1
+fmt.Println(v1,v2)
 }
